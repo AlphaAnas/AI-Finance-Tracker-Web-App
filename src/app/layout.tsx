d80f12399574dd8f7dbe8c0bc2b290429b28e737
muhd.anas70pe@gmail.com <96944434+AlphaAnas@users.noreply.github.com>
2025-04-12 15:24:49 +0500

    Fix: Sidebar not displaying on Landing page now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,19 +8,25 @@ import { Toaster } from 'react-hot-toast';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
-  const isAuthPage = pathname === '/login' || pathname === '/signup';
+
+  // Define an array of paths where the sidebar should not be displayed
+  const excludedPaths = ['/login', '/signup', '/landingpage'];
+
+  // Check if the current path is in the excluded paths list
+  const shouldDisplaySidebar = !excludedPaths.includes(pathname);
 
   return (
     <html lang="en">
       <body>
         <Toaster position="top-center" />
         <div className="flex h-screen">
-          {!isAuthPage && (
+          {/* Conditionally render the sidebar */}
+          {shouldDisplaySidebar && (
             <div className="w-1/5 min-w-40 max-w-60">
               <Sidebar />
             </div>
           )}
-          <div className={`${!isAuthPage ? 'flex-1' : 'w-full'} overflow-y-auto bg-gray-100`}>
+          <div className={`${shouldDisplaySidebar ? 'flex-1' : 'w-full'} overflow-y-auto bg-gray-100`}>
             {children}
           </div>
         </div>
